c7ba883ccd76923728f962fc39b1663bd1676f49
Thomas Harris <teh019283@gmail.com>
2020-04-08 11:23:46 +0200

    Add some basic tests, one unit and one controller to start

---
+++ src/test/kotlin/com/healthmetrix/labres/order/OrderNumberTest.kt
@@ -0,0 +1,16 @@
+package com.healthmetrix.labres.order
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class OrderNumberTest {
+
+    @Test
+    fun `randomly generated IDs match the form of external order numbers`() {
+        val allMatch = (0 until 100)
+            .map { OrderNumber.random() }
+            .all { it.externalOrderNumber.matches(Regex("^[0123456789abcdefghikmnpqrstuvwxyz]{8}$")) }
+
+        assertThat(allMatch).isEqualTo(true)
+    }
+}
