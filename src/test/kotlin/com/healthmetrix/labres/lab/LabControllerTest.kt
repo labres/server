4f1ba0c7fdd0134f7630328476d5286cf241bd60
Thomas Harris <teh019283@gmail.com>
2020-04-17 11:58:54 +0200

    PR tweaks, Update tests to mock direct dependencies, and reorganize

--- src/test/kotlin/com/healthmetrix/labres/lab/LabControllerTest.kt
+++ src/test/kotlin/com/healthmetrix/labres/lab/LabControllerTest.kt
@@ -5,15 +5,9 @@ import com.healthmetrix.labres.LabResTestApplication
 import com.healthmetrix.labres.encodeBase64
 import com.healthmetrix.labres.order.OrderNumber
 import com.healthmetrix.labres.order.Status
-import com.healthmetrix.labres.persistence.OrderInformation
-import com.healthmetrix.labres.persistence.OrderInformationRepository
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
-import io.mockk.verify
-import java.time.Instant
-import java.util.Date
-import java.util.UUID
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -37,120 +31,100 @@ class LabControllerTest {
     private lateinit var objectMapper: ObjectMapper
 
     @MockkBean
-    private lateinit var orderInformationRepository: OrderInformationRepository
+    private lateinit var updateResultUseCase: UpdateResultUseCase
 
     @MockkBean
     private lateinit var extractObxResultUseCase: ExtractObxResultUseCase
 
-    private val now = Date.from(Instant.now())
+    @Nested
+    inner class JSON {
+        @Test
+        fun `uploading a valid json request body with an external order number returns 200`() {
 
-    @BeforeEach
-    fun beforeEach() {
-        every { orderInformationRepository.save(any()) } answers { this.value }
-    }
+            every { updateResultUseCase(any(), any()) } returns UpdateStatusResponse.Success
 
-    @Test
-    fun `uploading a valid json doc with an external order number returns 200`() {
-
-        val orderNumber = OrderNumber.External.random()
-
-        every { orderInformationRepository.findByOrderNumber(any()) } returns
-                OrderInformation(UUID.randomUUID(), orderNumber, Status.IN_PROGRESS, now, null)
-
-        mockMvc.put("/v1/orders/result/json") {
-            header(HttpHeaders.AUTHORIZATION, "labId")
-            contentType = MediaType.APPLICATION_JSON
-            content = objectMapper.writeValueAsString(
-                mapOf(
-                    "externalOrderNumber" to "0123456789",
-                    "result" to Status.NEGATIVE
+            mockMvc.put("/v1/orders/result/json") {
+                header(HttpHeaders.AUTHORIZATION, "labId")
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(
+                    mapOf(
+                        "externalOrderNumber" to "0123456789",
+                        "result" to Status.NEGATIVE
+                    )
                 )
-            )
-        }.andExpect {
-            status { isOk }
+            }.andExpect {
+                status { isOk }
+            }
         }
 
-        verify(exactly = 1) {
-            orderInformationRepository.save(match {
-                it.number == orderNumber &&
-                        it.status == Status.NEGATIVE
-            })
-        }
-    }
+        @Test
+        fun `upload a document to an unknown external order number returns 404`() {
+            every { updateResultUseCase(any(), any()) } returns UpdateStatusResponse.OrderNotFound
 
-    @Test
-    fun `upload a document to an unknown external order number returns 404`() {
-        every { orderInformationRepository.findByOrderNumber(any()) } returns null
-        mockMvc.put("/v1/orders/result/json") {
-            contentType = MediaType.APPLICATION_JSON
-            header(HttpHeaders.AUTHORIZATION, "labId")
-            content = objectMapper.writeValueAsBytes(
-                mapOf(
-                    "externalOrderNumber" to "0123456789",
-                    "result" to Status.POSITIVE
+            mockMvc.put("/v1/orders/result/json") {
+                contentType = MediaType.APPLICATION_JSON
+                header(HttpHeaders.AUTHORIZATION, "labId")
+                content = objectMapper.writeValueAsBytes(
+                    mapOf(
+                        "externalOrderNumber" to "0123456789",
+                        "result" to Status.POSITIVE
+                    )
                 )
-            )
-        }.andExpect {
-            status { isNotFound }
+            }.andExpect {
+                status { isNotFound }
+            }
         }
-    }
 
-    @Test
-    fun `upload a document to an invalid external order number returns 404`() {
-        every { orderInformationRepository.findByOrderNumber(any()) } returns null
-        mockMvc.put("/v1/orders/result/json") {
-            header(HttpHeaders.AUTHORIZATION, "labId")
-            contentType = MediaType.APPLICATION_JSON
-            content = objectMapper.writeValueAsBytes(
-                mapOf(
-                    "externalOrderNumber" to "nonsense",
-                    "result" to Status.POSITIVE
+        @Test
+        fun `upload a document to an invalid external order number returns 404`() {
+            mockMvc.put("/v1/orders/result/json") {
+                header(HttpHeaders.AUTHORIZATION, "labId")
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsBytes(
+                    mapOf(
+                        "externalOrderNumber" to "nonsense",
+                        "result" to Status.POSITIVE
+                    )
                 )
-            )
-        }.andExpect {
-            status { isNotFound }
+            }.andExpect {
+                status { isNotFound }
+            }
         }
     }
 
-    @Test
-    fun `uploading a pretend OBX message returns 200`() {
-        val orderNumber = OrderNumber.External.random()
+    @Nested
+    inner class OBX {
 
-        every { orderInformationRepository.findByOrderNumber(any()) } returns
-                OrderInformation(UUID.randomUUID(), orderNumber, Status.IN_PROGRESS, now, null)
-        every { extractObxResultUseCase(any(), any()) } returns
-                LabResult(OrderNumber.External.random(), Result.NEGATIVE)
+        @Test
+        fun `uploading a pretend OBX message returns 200`() {
+            every { updateResultUseCase(any(), any()) } returns
+                    UpdateStatusResponse.Success
+            every { extractObxResultUseCase(any(), any()) } returns
+                    LabResult(OrderNumber.External.random(), Result.NEGATIVE)
 
-        mockMvc.put("/v1/orders/result/obx") {
-            header(HttpHeaders.AUTHORIZATION, "Basic ${"user:pass".encodeBase64()}")
-            contentType = MediaType.TEXT_PLAIN
-            content = "NEGATIVE"
-        }.andExpect {
-            status { isOk }
+            mockMvc.put("/v1/orders/result/obx") {
+                header(HttpHeaders.AUTHORIZATION, "Basic ${"user:pass".encodeBase64()}")
+                contentType = MediaType.TEXT_PLAIN
+                content = "NEGATIVE"
+            }.andExpect {
+                status { isOk }
+            }
         }
 
-        verify(exactly = 1) {
-            orderInformationRepository.save(match {
-                it.number == orderNumber &&
-                        it.status == Status.NEGATIVE
-            })
-        }
-    }
+        @Test
+        fun `uploading an invalid OBX message returns 500`() {
+            every { updateResultUseCase(any(), any()) } returns
+                    UpdateStatusResponse.InfoUnreadable
 
-    @Test
-    fun `uploading an invalid OBX message returns 500`() {
-        val orderNumber = OrderNumber.External.random()
+            every { extractObxResultUseCase(any(), any()) } returns null
 
-        every { orderInformationRepository.findByOrderNumber(any()) } returns
-                OrderInformation(UUID.randomUUID(), orderNumber, Status.IN_PROGRESS, now, null)
-        every { extractObxResultUseCase(any(), "labId") } returns null
-
-        mockMvc.put("/v1/orders/result/obx") {
-            header(HttpHeaders.AUTHORIZATION, "Basic ${"user:pass".encodeBase64()}")
-            contentType = MediaType.TEXT_PLAIN
-            content = "NOT OBX"
-        }.andExpect {
-            status { isInternalServerError }
+            mockMvc.put("/v1/orders/result/obx") {
+                header(HttpHeaders.AUTHORIZATION, "Basic ${"user:pass".encodeBase64()}")
+                contentType = MediaType.TEXT_PLAIN
+                content = "NOT OBX"
+            }.andExpect {
+                status { isInternalServerError }
+            }
         }
     }
 }
