05958314e4441025efafad549d0ce82329187712
Varun Jayaraman <varun.jayaraman@gmail.com>
2020-04-22 13:05:18 +0200

    Make info configurable

--- src/main/kotlin/com/healthmetrix/labres/SwaggerConfig.kt
+++ src/main/kotlin/com/healthmetrix/labres/SwaggerConfig.kt
@@ -3,25 +3,44 @@ package com.healthmetrix.labres
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class SwaggerConfig {
+class SwaggerConfig(
+    private val documentationInfo: DocumentationInfo
+) {
     @Bean
     fun api(): OpenAPI {
         return OpenAPI()
-            .info(apiInfo())
+            .info(documentationInfo.toApiInfo())
     }
+}
 
-    private fun apiInfo(): Info {
+@ConfigurationProperties(prefix = "documentation-info")
+@ConstructorBinding
+data class DocumentationInfo(
+    val title: String,
+    val version: String,
+    val description: String,
+    val contact: DocumentationContact
+) {
+    data class DocumentationContact(
+        val name: String,
+        val url: String,
+        val email: String
+    )
+
+    fun toApiInfo(): Info {
         return Info()
-            .title("lab-res API - Beta")
-            .version("1")
-            .description("LabRes provides an API to establish a direct connection between laboratory results and citizens.")
+            .title(title)
+            .version(version)
+            .description(description)
             .contact(Contact()
-                .name("Healthmetrix GmbH")
-                .url("https://www.labres.de")
-                .email("admin@healthmetrix.com"))
+                .url(contact.url)
+                .name(contact.name)
+                .email(contact.email))
     }
 }
