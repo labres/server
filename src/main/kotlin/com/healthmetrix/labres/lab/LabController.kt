ec85d7257ba8cf339707545a35db5cc0bcbc468f
Varun Jayaraman <varun.jayaraman@gmail.com>
2020-04-23 16:35:42 +0200

    Document lab controller (#43)
    * Document json result endpoint
    
    * Document OBX result endpoint
    
    Document response payload media type
    
    * Document test type in request body
    
    * Document LDT endpoint

--- src/main/kotlin/com/healthmetrix/labres/lab/LabController.kt
+++ src/main/kotlin/com/healthmetrix/labres/lab/LabController.kt
@@ -1,9 +1,18 @@
 package com.healthmetrix.labres.lab
 
+import com.healthmetrix.labres.LABORATORY_API_TAG
 import com.healthmetrix.labres.LabResApiResponse
 import com.healthmetrix.labres.asEntity
 import com.healthmetrix.labres.order.OrderNumber
 import com.healthmetrix.labres.order.Status
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.headers.Header
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -14,6 +23,18 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@ApiResponses(
+    value = [
+        ApiResponse(
+            responseCode = "401",
+            description = "API key invalid or missing",
+            headers = [Header(name = "WWW-Authenticate", schema = Schema(type = "string"))],
+            content = [Content()]
+        )
+    ]
+)
+@SecurityRequirement(name = "LabCredential")
+@Tag(name = LABORATORY_API_TAG)
 class LabController(
     private val extractLabIdUseCase: ExtractLabIdUseCase,
     private val extractObxResultUseCase: ExtractObxResultUseCase,
@@ -23,7 +44,29 @@ class LabController(
 
     @PutMapping(
         path = ["/v1/results/json"],
-        consumes = [MediaType.APPLICATION_JSON_VALUE]
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    @Operation(
+        summary = "Upload lab result via JSON"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Result uploaded successfully",
+                content = [
+                    Content(schema = Schema(implementation = UpdateStatusResponse.Success::class, hidden = true))
+                ]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "No order for order number found",
+                content = [
+                    Content(schema = Schema(implementation = UpdateStatusResponse.OrderNotFound::class, hidden = true))
+                ]
+            )
+        ]
     )
     fun jsonResult(
         @RequestHeader(HttpHeaders.AUTHORIZATION)
@@ -48,12 +91,46 @@ class LabController(
 
     @PutMapping(
         path = ["/v1/results/obx"],
-        consumes = [MediaType.TEXT_PLAIN_VALUE]
+        consumes = [MediaType.TEXT_PLAIN_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    @Operation(
+        summary = "Upload lab result via OBX text message"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Result uploaded successfully",
+                content = [
+                    Content(schema = Schema(implementation = UpdateStatusResponse.Success::class, hidden = true))
+                ]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Invalid OBX Message. See https://wiki.hl7.de/index.php?title=Segment_OBX for a description of acceptable fields",
+                content = [
+                    Content(schema = Schema(implementation = UpdateStatusResponse.InfoUnreadable::class, hidden = true))
+                ]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "No order for order number found",
+                content = [
+                    Content(schema = Schema(implementation = UpdateStatusResponse.OrderNotFound::class, hidden = true))
+                ]
+            )
+        ]
     )
     fun obxResult(
         @RequestHeader(HttpHeaders.AUTHORIZATION)
         labIdHeader: String,
         @RequestBody
+        @Schema(
+            description = "An OBX Segment of an HL7 ORU R1 message (see https://wiki.hl7.de/index.php?title=Segment_OBX for details).",
+            example = "OBX|3|ST|21300^2019-nCoronav.-RNA Sonst (PCR)|0061749799|Positiv|||N|||S|||20200406101220|Extern|||||||||Extern",
+            format = "string"
+        )
         obxMessage: String
     ): ResponseEntity<UpdateStatusResponse> {
         val labId = extractLabIdUseCase(labIdHeader)
@@ -67,12 +144,42 @@ class LabController(
 
     @PutMapping(
         path = ["/v1/results/ldt"],
-        consumes = [MediaType.TEXT_PLAIN_VALUE]
+        consumes = [MediaType.TEXT_PLAIN_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    @Operation(
+        summary = "Upload lab result via LDT Document"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Result uploaded successfully",
+                content = [
+                    Content(schema = Schema(implementation = UpdateStatusResponse.Success::class, hidden = true))
+                ]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Invalid LDT Document",
+                content = [
+                    Content(schema = Schema(implementation = UpdateStatusResponse.InfoUnreadable::class, hidden = true))
+                ]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "No order for order number found",
+                content = [
+                    Content(schema = Schema(implementation = UpdateStatusResponse.OrderNotFound::class, hidden = true))
+                ]
+            )
+        ]
     )
     fun ldtResult(
         @RequestHeader(HttpHeaders.AUTHORIZATION)
         labIdHeader: String,
         @RequestBody
+        @Schema(description = "An LDT Document")
         ldtMessage: String
     ): ResponseEntity<UpdateStatusResponse> {
         val labId = extractLabIdUseCase(labIdHeader)
@@ -86,9 +193,18 @@ class LabController(
 }
 
 data class JsonResult(
+    @Schema(
+        description = "The external order number",
+        example = "1234567890"
+    )
     val orderNumber: String,
-    val result: Result,
-    val type: String? = null
+    @Schema(
+        description = "The kind of test used to generate the given result as a LOINC code.",
+        example = "94500-6"
+    )
+    val type: String? = null,
+    @Schema(description = "The test result")
+    val result: Result
 )
 
 data class LabResult(
@@ -122,7 +238,8 @@ enum class Result {
     }
 }
 
-sealed class UpdateStatusResponse(httpStatus: HttpStatus, hasBody: Boolean = false) : LabResApiResponse(httpStatus, hasBody) {
+sealed class UpdateStatusResponse(httpStatus: HttpStatus, hasBody: Boolean = false) :
+    LabResApiResponse(httpStatus, hasBody) {
     object Success : UpdateStatusResponse(HttpStatus.OK)
     object OrderNotFound : UpdateStatusResponse(HttpStatus.NOT_FOUND)
     object InfoUnreadable : UpdateStatusResponse(HttpStatus.BAD_REQUEST, true) {
