60c06b0971a85596e3c170ae014eb6aaa2ee5107
Thomas Harris <teh019283@gmail.com>
2020-04-28 17:35:34 +0200

    Special case some exception handling to provide more useful messages
    Things to consider for the future:
    - Always include an incidentId in the error response body, as another key?
    - Tweak some errors to conform to REST standards, for example,
      report HttpMessageNotReadableExceptions as BAD_REQUESTs?

--- src/main/kotlin/com/healthmetrix/labres/GlobalErrorHandler.kt
+++ src/main/kotlin/com/healthmetrix/labres/GlobalErrorHandler.kt
@@ -4,6 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema
 import java.util.UUID
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.web.HttpRequestMethodNotSupportedException
+import org.springframework.web.bind.MissingRequestHeaderException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -13,17 +17,39 @@ class GlobalErrorHandler {
 
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun onException(exception: Exception): ResponseEntity<InternalServerError> {
+    fun onException(exception: Exception) = handle(exception)
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun onMethodNotSupported(exception: HttpRequestMethodNotSupportedException) =
+        handle(exception, exception.message ?: "Request method not supported")
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun onContentTypeNotSupported(exception: HttpMediaTypeNotSupportedException) =
+        handle(exception, exception.message ?: "Content type not supported")
+
+    @ExceptionHandler(MissingRequestHeaderException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun onMissingRequestHeader(exception: MissingRequestHeaderException) =
+        handle(exception, "Missing request header '${exception.headerName}'")
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun onHttpMessageNotReadable(exception: HttpMessageNotReadableException) =
+        handle(exception, "Malformed request body. Cause: ${exception.cause?.javaClass?.simpleName ?: "unknown"}")
+
+    private fun handle(exception: Exception, msg: String? = null): ResponseEntity<InternalServerError> {
         val id = UUID.randomUUID()
         logger.error("Exception caught incidentId=$id", exception)
-        return InternalServerError("$id").asEntity()
+        return InternalServerError(msg ?: id.toString()).asEntity()
     }
 
     @Schema(description = "General error response body")
     data class InternalServerError(
         @Schema(
-            description = "A summary of the error that occurred with an incident ID",
-            example = "Exception caught incidentId=1234 Error: Internal Server Error"
+            description = "A summary of the error that occurred, or an incident ID if the error is unexpected",
+            example = "Malformed request body. Cause: JsonParseException"
         )
         val message: String
     ) : LabResApiResponse(HttpStatus.INTERNAL_SERVER_ERROR)
