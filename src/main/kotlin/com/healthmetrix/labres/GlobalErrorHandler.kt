a68550db0b62068b99dcac97602469f73da45589
Thomas Harris <teh019283@gmail.com>
2020-04-29 17:38:29 +0200

    Tweak error status codes to be more accurate
    The special cases in a previous PR are now 400 instead of 500

--- src/main/kotlin/com/healthmetrix/labres/GlobalErrorHandler.kt
+++ src/main/kotlin/com/healthmetrix/labres/GlobalErrorHandler.kt
@@ -1,6 +1,7 @@
 package com.healthmetrix.labres
 
 import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import java.util.UUID
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -17,40 +18,53 @@ class GlobalErrorHandler {
 
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun onException(exception: Exception) = handle(exception)
-
-    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun onMethodNotSupported(exception: HttpRequestMethodNotSupportedException) =
-        handle(exception, exception.message ?: "Request method not supported")
-
-    @ExceptionHandler(HttpMediaTypeNotSupportedException::class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun onContentTypeNotSupported(exception: HttpMediaTypeNotSupportedException) =
-        handle(exception, exception.message ?: "Content type not supported")
-
-    @ExceptionHandler(MissingRequestHeaderException::class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun onMissingRequestHeader(exception: MissingRequestHeaderException) =
-        handle(exception, "Missing request header '${exception.headerName}'")
-
-    @ExceptionHandler(HttpMessageNotReadableException::class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun onHttpMessageNotReadable(exception: HttpMessageNotReadableException) =
-        handle(exception, "Malformed request body. Cause: ${exception.cause?.javaClass?.simpleName ?: "unknown"}")
-
-    private fun handle(exception: Exception, msg: String? = null): ResponseEntity<InternalServerError> {
+    fun onException(exception: Exception): ResponseEntity<Error> {
         val id = UUID.randomUUID()
         logger.error("Exception caught incidentId=$id", exception)
-        return InternalServerError(msg ?: id.toString()).asEntity()
+        return Error.InternalServerError(id).asEntity()
     }
 
-    @Schema(description = "General error response body")
-    data class InternalServerError(
-        @Schema(
-            description = "A summary of the error that occurred, or an incident ID if the error is unexpected",
-            example = "Malformed request body. Cause: JsonParseException"
-        )
-        val message: String
-    ) : LabResApiResponse(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun onMethodNotSupported(exception: HttpRequestMethodNotSupportedException) =
+        badRequest(exception, exception.message ?: "Request method not supported")
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ApiResponse(description = "Error with the request")
+    fun onContentTypeNotSupported(exception: HttpMediaTypeNotSupportedException) =
+        badRequest(exception, exception.message ?: "Content type not supported")
+
+    @ExceptionHandler(MissingRequestHeaderException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun onMissingRequestHeader(exception: MissingRequestHeaderException) =
+        badRequest(exception, "Missing request header '${exception.headerName}'")
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun onHttpMessageNotReadable(exception: HttpMessageNotReadableException) =
+        badRequest(exception, "Malformed request body. Cause: ${exception.cause?.javaClass?.simpleName ?: "unknown"}")
+
+    private fun badRequest(exception: Exception, msg: String? = null): ResponseEntity<Error.BadRequest> {
+        val id = UUID.randomUUID()
+        logger.error("Exception caught incidentId=$id", exception)
+        return Error.BadRequest(msg ?: id.toString()).asEntity()
+    }
+
+    sealed class Error(status: HttpStatus) : LabResApiResponse(status) {
+        @Schema(description = "General error response body")
+        class InternalServerError(incidentId: UUID) : Error(HttpStatus.INTERNAL_SERVER_ERROR) {
+            @Schema(description = "An incident UUID for an unexpected error")
+            val message = incidentId.toString()
+        }
+
+        @Schema(description = "General error response body")
+        data class BadRequest(
+            @Schema(
+                description = "A summary of the error that occurred",
+                example = "Malformed request body. Cause: JsonParseException"
+            )
+            val message: String
+        ) : Error(HttpStatus.BAD_REQUEST)
+    }
 }
