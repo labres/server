1c5b31038b00a0a999d4498c9ff07d6fd67e44d7
khbnr <huebner.kevin@gmail.com>
2020-06-23 15:37:58 +0200

    Bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 plugins {
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
-    id("org.springframework.boot") version "2.3.0.RELEASE"
+    id("org.springframework.boot") version "2.3.1.RELEASE"
     id("com.github.ben-manes.versions") version "0.28.0"
 }
 
@@ -20,13 +20,13 @@ dependencies {
     implementation(kotlin("reflect"))
 
     // spring
-    val springBootVersion = "2.3.0.RELEASE"
+    val springBootVersion = "2.3.1.RELEASE"
     implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
     implementation("org.springframework.boot:spring-boot-starter-actuator:$springBootVersion")
     implementation("org.springframework.boot:spring-boot-starter-webflux:$springBootVersion")
 
     // structured logging
-    implementation("net.logstash.logback:logstash-logback-encoder:6.3")
+    implementation("net.logstash.logback:logstash-logback-encoder:6.4")
 
     // serialization
     val jacksonVersion = "2.11.0"
@@ -34,16 +34,16 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
 
     // swagger
-    val springdocVersion = "1.3.9"
+    val springdocVersion = "1.4.1"
     implementation("org.springdoc:springdoc-openapi-ui:$springdocVersion")
     implementation("org.springdoc:springdoc-openapi-kotlin:$springdocVersion")
 
     // spring data
     implementation("org.springframework.data:spring-data-commons:$springBootVersion")
-    implementation("io.github.boostchicken:spring-data-dynamodb:5.2.3")
+    implementation("io.github.boostchicken:spring-data-dynamodb:5.2.5")
 
     // dynamodb
-    val awsSdkVersion = "1.11.785"
+    val awsSdkVersion = "1.11.808"
     implementation(platform("com.amazonaws:aws-java-sdk-bom:$awsSdkVersion"))
     implementation("com.amazonaws:aws-java-sdk-dynamodb:$awsSdkVersion")
 
@@ -51,7 +51,7 @@ dependencies {
     implementation("com.amazonaws.secretsmanager:aws-secretsmanager-caching-java:1.0.1")
 
     // google fcm
-    implementation("com.google.firebase:firebase-admin:6.13.0")
+    implementation("com.google.firebase:firebase-admin:6.14.0")
 
     // testing
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
